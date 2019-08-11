15280131241892b81e6619ce7a98e496a3d1ba08
ArkadiuszDabkowski <arkadiusz.dabkowski92@gmail.com>
2019-08-11 23:14:33 +0200

    Added SearchHoliday and SearchFreeDay class

--- src/main/java/com/infoshareacademy/jjdd7/SearchHoliday.java
+++ src/main/java/com/infoshareacademy/jjdd7/SearchHoliday.java
@@ -1,10 +1,9 @@
 package com.infoshareacademy.jjdd7;
 
-import java.util.ArrayList;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class SearchHoliday {
 
@@ -22,15 +21,19 @@ public class SearchHoliday {
         ParserImpl parser = new ParserImpl("HolidaysApi.json");
         String name = scanner.nextLine();
         List<Holiday> myList = parser.getListOfHolidays();
+        boolean ifHolidayfound = false;
         for (Holiday holiday : myList) {
-            if (name.equals(holiday.getName()) || holiday.getName().contains(name)) {
-                System.out.println(holiday.getDate());
-            } else {
-                System.out.println("There is no holiday.");
+            if (name.toLowerCase().equals(holiday.getName()) || holiday.getName().toLowerCase().contains(name) && name.length() <= 3) {
+                System.out.println(holiday.getDate() + " " + holiday.getName());
+                ifHolidayfound = true;
                 break;
             }
         }
+        if(!ifHolidayfound){
+            System.out.println("There is no holiday");
+        }
     }
 }
+
 
 
