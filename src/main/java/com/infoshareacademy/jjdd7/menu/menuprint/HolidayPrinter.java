36e9d0762e0ad3a546937719bf2bd383437b9c04
Maciej Dzieciuch <macidzie@gmail.com>
2019-08-12 09:50:28 +0200

    All bugs fixed

--- src/main/java/com/infoshareacademy/jjdd7/menu/menuprint/HolidayPrinter.java
+++ src/main/java/com/infoshareacademy/jjdd7/menu/menuprint/HolidayPrinter.java
@@ -7,7 +7,7 @@ public class HolidayPrinter implements Menu {
     @Override
     public void doAction() {
         System.out.println("\n1. Find holiday by name");
-        System.out.println("2. Enter date range");
+        System.out.println("2. Find holiday by date");
         System.out.println("0. Return");
     }
 }
