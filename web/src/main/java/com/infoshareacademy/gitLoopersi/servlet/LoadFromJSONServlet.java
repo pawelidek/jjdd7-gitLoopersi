b315065bc221a70b73720f0055dd1638d28f482a
Paweł Idek <pawelidek@op.pl>
2019-09-02 21:54:32 +0200

    Add empty servlets to fill with code in the future

---
+++ web/src/main/java/com/infoshareacademy/gitLoopersi/servlet/LoadFromJSONServlet.java
@@ -0,0 +1,23 @@
+package com.infoshareacademy.gitLoopersi.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/json-load")
+public class LoadFromJSONServlet extends HttpServlet {
+
+  private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    //TODO
+  }
+
+}
---
+++ web/src/main/java/com/infoshareacademy/gitLoopersi/servlet/VacationAcceptServlet.java
@@ -0,0 +1,35 @@
+package com.infoshareacademy.gitLoopersi.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/vacation-accept")
+public class VacationAcceptServlet extends HttpServlet {
+
+  private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    //TODO
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    //TODO
+  }
+
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    //TODO
+  }
+
+}
