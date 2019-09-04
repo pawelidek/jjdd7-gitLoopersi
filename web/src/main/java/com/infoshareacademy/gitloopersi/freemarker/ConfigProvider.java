f01b71961659af480c5c7cb1570e7fdcf4495352
Marek Sitarski <marek_sitarski@vp.pl>
2019-09-04 23:22:46 +0200

    Initial

--- web/src/main/java/com/infoshareacademy/gitloopersi/freemarker/ConfigProvider.java
+++ web/src/main/java/com/infoshareacademy/gitloopersi/freemarker/ConfigProvider.java
@@ -7,21 +7,6 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ConfigProvider {
 
-<<<<<<< HEAD
-    private Configuration configuration;
-
-    public Configuration getConfiguration() {
-        if (configuration == null) {
-            configuration = new Configuration(Configuration.VERSION_2_3_29);
-            configuration.setDefaultEncoding("UTF-8");
-            configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-            configuration.setLogTemplateExceptions(false);
-            configuration.setWrapUncheckedExceptions(true);
-        }
-        return configuration;
-    }
-}
-=======
   private Configuration configuration;
 
   public Configuration getConfiguration() {
@@ -36,4 +21,3 @@ public class ConfigProvider {
     return configuration;
   }
 }
->>>>>>> 5bff9a63a26d39dfc7c65fc96a7ffdeb1c7b1cf3
