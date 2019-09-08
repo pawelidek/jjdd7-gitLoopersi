f5d6851b482fcc4d6958590a1561557069fed4e5
Paweł Idek <pawelidek@op.pl>
2019-09-08 14:34:05 +0200

    Initial commit

--- web/src/main/java/com/infoshareacademy/gitloopersi/servlet/admin/EmployeeManagerServlet.java
+++ web/src/main/java/com/infoshareacademy/gitloopersi/servlet/admin/EmployeeManagerServlet.java
@@ -1,11 +1,21 @@
 package com.infoshareacademy.gitloopersi.servlet.admin;
 
+import com.infoshareacademy.gitloopersi.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.registry.infomodel.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +24,28 @@ public class UserManagerServlet extends HttpServlet {
 
   private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
+  @Inject
+  private TemplateProvider templateProvider;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    //TODO
+
+    Template template = templateProvider.getTemplate(getServletContext(), "user-manager.ftlh");
+
+    List<User> employeeList = new ArrayList<>();
+    employeeList =
+
+    Map<String, Object> dataModel = new HashMap<>();
+    dataModel.put("employeeList", employeeList);
+
+    PrintWriter printWriter = resp.getWriter();
+
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      logger.error(e.getMessage());
+    }
   }
 
   @Override
