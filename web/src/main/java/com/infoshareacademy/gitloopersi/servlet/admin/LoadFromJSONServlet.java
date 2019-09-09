241dcc47997ea0bb7484ce3fbd4bc5fc24e2a76e
Maciej Dzieciuch <macidzie@gmail.com>
2019-09-09 17:00:44 +0200

    Add form and bean for upload file

--- web/src/main/java/com/infoshareacademy/gitloopersi/servlet/admin/LoadFromJSONServlet.java
+++ web/src/main/java/com/infoshareacademy/gitloopersi/servlet/admin/LoadFromJSONServlet.java
@@ -1,7 +1,15 @@
 package com.infoshareacademy.gitloopersi.servlet.admin;
 
+import com.infoshareacademy.gitloopersi.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,14 +18,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebServlet("/admin/load/json")
+@MultipartConfig
 public class LoadFromJSONServlet extends HttpServlet {
 
   private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
+  @Inject
+  private TemplateProvider templateProvider;
+
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    //TODO
+
+    Template template = templateProvider.getTemplate(getServletContext(), "home.ftlh");
+
+    Map<String, Object> dataModel = new HashMap<>();
+    dataModel.put("function", "FileLoader");
+
+    PrintWriter printWriter = resp.getWriter();
+
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      logger.error(e.getMessage());
+    }
+
   }
 
 }
