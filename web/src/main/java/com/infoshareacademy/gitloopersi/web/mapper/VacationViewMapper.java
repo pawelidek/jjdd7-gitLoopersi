17a21879dbddda9be650eba457068c0729cbe6ee
Maciej Dzieciuch <macidzie@gmail.com>
2019-09-19 23:12:41 +0200

    Add popup and view list all vacations

---
+++ web/src/main/java/com/infoshareacademy/gitloopersi/web/mapper/VacationViewMapper.java
@@ -0,0 +1,30 @@
+package com.infoshareacademy.gitloopersi.web.mapper;
+
+import com.infoshareacademy.gitloopersi.domain.entity.Vacation;
+import com.infoshareacademy.gitloopersi.web.view.VacationView;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.transaction.Transactional;
+
+@Stateless
+public class VacationViewMapper {
+
+  @EJB
+  private EmployeeViewMapper employeeViewMapper;
+
+  @Transactional
+  public VacationView mapEntityToView(Vacation vacation) {
+
+    VacationView vacationView = new VacationView();
+
+    vacationView.setId(vacation.getId());
+    vacationView.setEmployee(employeeViewMapper.mapEntityToView(vacation.getEmployee()));
+    vacationView.setDateFrom(vacation.getDateFrom());
+    vacationView.setDateTo(vacation.getDateTo());
+    vacationView.setDaysCount(vacation.getDaysCount());
+    vacationView.setVacationType(vacation.getVacationType());
+    vacationView.setStatusType(vacation.getStatusType());
+
+    return vacationView;
+  }
+}
