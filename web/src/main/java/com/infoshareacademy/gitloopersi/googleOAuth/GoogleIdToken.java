ca010ecc5c868fead9db476783d518dd87186f96
ArkadiuszDabkowski <arkadiusz.dabkowski92@gmail.com>
2019-09-20 15:19:51 +0200

    Add a logout function

---
+++ web/src/main/java/com/infoshareacademy/gitloopersi/googleOAuth/GoogleIdToken.java
@@ -0,0 +1,19 @@
+package com.infoshareacademy.gitloopersi.googleOAuth;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import java.util.Collections;
+
+public class GoogleIdToken {
+
+
+
+
+
+  }
+
+
+
+
+
+
