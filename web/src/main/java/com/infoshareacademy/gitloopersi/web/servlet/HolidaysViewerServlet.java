d291714ebd3b7ffac891a3d19dc928b553719ca2
Paweł Idek <pawelidek@op.pl>
2019-09-26 16:32:26 +0200

    Fix unavailable menu positions

---
+++ web/src/main/java/com/infoshareacademy/gitloopersi/web/servlet/HolidaysViewerServlet.java
@@ -0,0 +1,54 @@
+package com.infoshareacademy.gitloopersi.web.servlet;
+
+import com.infoshareacademy.gitloopersi.domain.entity.Holiday;
+import com.infoshareacademy.gitloopersi.domain.model.Calendar;
+import com.infoshareacademy.gitloopersi.freemarker.TemplateProvider;
+import com.infoshareacademy.gitloopersi.service.calendarmanager.CalendarService;
+import com.infoshareacademy.gitloopersi.service.holidaymanager.HolidayService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/holiday")
+public class HolidaysViewerServlet extends HttpServlet {
+
+  @Inject
+  HolidayService holidayService;
+  @Inject
+  TemplateProvider templateProvider;
+  private Logger logger = LoggerFactory.getLogger(getClass().getName());
+  @Inject
+  private CalendarService calendarService;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    Map<String, Object> dataModel = new HashMap<>();
+    List<Holiday> holidays = holidayService.findAllHolidays();
+    List<Calendar> dates = calendarService.findAllHolidaysDates();
+
+    dataModel.put("userType", req.getSession().getAttribute("userType"));
+    dataModel.put("holidays", holidays);
+    dataModel.put("function", "HolidaysViewer");
+    dataModel.put("dates", dates);
+    PrintWriter printWriter = resp.getWriter();
+    Template template = templateProvider.getTemplate(getServletContext(), "home.ftlh");
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      logger.error(e.getMessage());
+    }
+  }
+
+}
